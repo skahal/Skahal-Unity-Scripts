c95701e9999190305f043cac88232e1c79c5155d
Diego Giacomelli <giacomelli@gmail.com>
2016-06-10 19:24:01 -0300

    Added methods With and ToAcronym.

---
+++ src/Editor/UnitTests/Text/StringHelperTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Shakal.Text.UnitTests
+{
+    public class StringHelperTest
+    {
+        [Test]
+        public void ToAcroynm_OneWord_OneLetter()
+        {
+            var actual = "Skahal".ToAcronym();
+            Assert.AreEqual("S", actual);
+        }
+
+        [Test]
+        public void ToAcroynm_TwoWords_TwoLetters()
+        {
+            var actual = "Skahal Studios".ToAcronym();
+            Assert.AreEqual("SS", actual);
+        }
+
+        [Test]
+        public void ToAcroynm_MoreThanOneSeparator_Letters()
+        {
+            var actual = "Ska.hal Studios".ToAcronym(3, 2, ".", " ");
+            Assert.AreEqual("SHS", actual);
+        }
+
+
+        [Test]
+        public void ToAcroynm_MaxLength_TwoLetters()
+        {
+            var actual = "Skahal Studios Company".ToAcronym(2);
+            Assert.AreEqual("SS", actual);
+        }
+
+        [Test]
+        public void ToAcroynm_MinWordLength_TwoLetters()
+        {
+            var actual = "José de Souza".ToAcronym(2, 3);
+            Assert.AreEqual("JS", actual);
+        }
+    }
+}
--- src/Text/StringHelper.cs
+++ src/Text/StringHelper.cs
@@ -1,3 +1,5 @@
+using System.Globalization;
+using System.Linq;
 using UnityEngine;
 
 public static class StringHelper
@@ -16,6 +18,43 @@ public static class StringHelper
     public static string FormatSingularOrPlural(int counter, string formatSingular, string formatPlural, params object[] args)
     {
         return System.String.Format(Mathf.Abs(counter) > 1 ? formatPlural : formatSingular, args);
+    }
+
+    /// <summary>
+    /// A shortcut to string.Format(CultureInfo.InvariantCulture.
+    /// </summary>
+    /// <param name="value">The value.</param>
+    /// <param name="args">The arguments.</param>
+    /// <returns>The formatted string</returns>
+    public static string With(this string value, params object[] args)
+    {
+        return string.Format(CultureInfo.InvariantCulture, value, args);
+    }
+
+    /// <summary>
+    /// Converts string to acroynm.
+    /// </summary>
+    /// <param name="value">The value.</param>
+    /// <param name="maxAcronymLength">The max acronym length.</param>
+    /// <param name="minWordLength">The min word length to consider in acronim.</param>
+    /// <param name="wordSeparators">The word separators. Default is space.</param>
+    /// <returns>The acronum.</returns>
+   /// <exception cref="System.NotImplementedException"></exception>
+    public static string ToAcronym(this string value, int maxAcronymLength = 2, int minWordLength = 3, params string[] wordSeparators)
+    {
+        if (wordSeparators.Length == 0)
+        {
+            wordSeparators = new string[] { " " };
+        }
+
+        return string.Join(
+            "",  
+            value
+            .Split(wordSeparators, System.StringSplitOptions.RemoveEmptyEntries)
+            .Where(w => w.Length >= minWordLength)
+            .Select(w => w[0].ToString().ToUpperInvariant())
+            .Take(maxAcronymLength)
+            .ToArray());
     }
     #endregion
 }
