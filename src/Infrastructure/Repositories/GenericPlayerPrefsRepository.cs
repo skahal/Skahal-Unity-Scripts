ae31c9511b51b6d2ed1fc615bfb069a396be89e1
Diego Giacomelli <giacomelli@gmail.com>
2016-06-23 23:04:49 -0300

    Create GenericPlayerPrefsRepository.

---
+++ src/Infrastructure/Repositories/GenericPlayerPrefsRepository.cs
@@ -0,0 +1,13 @@
+﻿using Skahal.Infrastructure.Framework.Domain;
+
+namespace Skahal.Infrastructure.Repositories
+{
+    /// <summary>
+    /// PlayerPrefs genertic repository.
+    /// </summary>
+    /// <typeparam name="TEntity">The type of the entity.</typeparam>
+    public class GenericPlayerPrefsRepository<TEntity> : PlayerPrefsRepositoryBase<TEntity>
+         where TEntity : class, IAggregateRoot, new()
+    {		
+	}
+}
