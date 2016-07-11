37426c065d1c0c1e5d570bb0d7f29a66fd75d811
Diego Giacomelli <giacomelli@gmail.com>
2016-07-10 22:42:12 -0300

    Added Throw.AnyNull.

---
+++ src/Common/Throw.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Skahal.Common
+{
+	public static class Throw
+	{
+		public static void AnyNull(object target)
+		{
+			if (target == null) {
+				throw new ArgumentNullException ("target");
+			}
+
+			var properties = target.GetType ().GetProperties ();
+
+			foreach (var p in properties) {
+				if (p.GetValue (target, null) == null) {
+					throw new ArgumentNullException (p.Name);
+				}
+			}
+		}
+	}
+}
---
+++ src/Editor/UnitTests/Common/ThrowTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Skahal.Common.UnitTests
+{
+	[TestFixture]
+	public class ThrowTest
+	{
+		[Test]
+		public void AnyNull_AllPropertiesNotNull_NoException()
+		{
+			Throw.AnyNull (new { a = 1, b = "2", c = 3l });
+		}
+
+		[Test]
+		public void AnyNull_AnyPropertieNull_Exception()
+		{
+			Assert.Catch<ArgumentNullException> (() => {
+				Throw.AnyNull (new { a = (string) null, b = "2", c = 3l });
+			});
+
+			Assert.Catch<ArgumentNullException> (() => {
+				Throw.AnyNull (new { a = 1, b = (object) null, c = 3l });
+			});
+
+			Assert.Catch<ArgumentNullException> (() => {
+				Throw.AnyNull (new { a = 1, b = "2", c = (string) null });
+			});
+		}
+	}
+}
+
